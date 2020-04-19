8424e4484e2f5bb45349a4bcd6550b01c532f8d5
zykzml7788 <ykzhang@creams.io>
2020-04-19 23:52:35 +0800

    解决服务端404

--- temo-web/src/main/java/com/creams/temo/temoWebApplication.java
+++ temo-web/src/main/java/com/creams/temo/temoWebApplication.java
@@ -4,9 +4,17 @@ package com.creams.temo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.creams.temo")
-public class temoWebApplication {
+public class temoWebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // 解决服务端tomcat 404问题
+        return application.sources(temoWebApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(temoWebApplication.class, args);
     }
