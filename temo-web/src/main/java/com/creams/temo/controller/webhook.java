09777081de54ba72180122047ef4adbe91ca8dd6
haoqiang.zheng <haoqiang.zheng@bianlifeng.com>
2020-07-13 21:28:09 +0800

    test

--- temo-web/src/main/java/com/creams/temo/controller/webhook.java
+++ temo-web/src/main/java/com/creams/temo/controller/webhook.java
@@ -2,6 +2,9 @@ package com.creams.temo.controller;
 
 
 import com.creams.temo.entity.result.JsonResult;
+import com.creams.temo.tools.WebSocketUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/webhook")
 public class webhook {
-
+    private static Logger logger = LoggerFactory.getLogger(webhook.class);
     @PostMapping("/gitlab")
     public JsonResult gitlab(@RequestBody String content){
+        //提交测试
+
+        logger.info(content);
         return new JsonResult("success",200,content,true);
     }
 
